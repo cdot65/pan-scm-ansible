cd63393c9481aa675be7b429eabdde00dcc58e71
cdot65 <calvin@cdot.io>
2025-03-24 03:43:10 -0500

    Add documentation and module for HIP object management
    Introduced two modules: `hip_object` for managing HIP objects and `hip_object_info` for retrieving their information in Palo Alto Networks' SCM. Comprehensive user documentation, usage examples, and error-handling practices are included to aid users in creating, updating, deleting, or querying HIP objects.

--- plugins/modules/hip_object.py
+++ plugins/modules/hip_object.py
@@ -24,14 +24,14 @@ __metaclass__ = type
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.common.text.converters import to_text
-
 from ansible_collections.cdot65.scm.plugins.module_utils.api_spec.hip_object import HIPObjectSpec
+from scm.exceptions import InvalidObjectError, NameNotUniqueError, ObjectNotPresentError
+from scm.models.objects import HIPObjectUpdateModel
+
 from ansible_collections.cdot65.scm.plugins.module_utils.authenticate import get_scm_client
 from ansible_collections.cdot65.scm.plugins.module_utils.serialize_response import (
     serialize_response,
 )
-from scm.exceptions import InvalidObjectError, NameNotUniqueError, ObjectNotPresentError
-from scm.models.objects import HIPObjectUpdateModel
 
 DOCUMENTATION = r"""
 ---
@@ -426,7 +426,7 @@ EXAMPLES = r"""
             is_installed: true
             encrypted_locations:
               - name: "C:"
-                encryption_state: 
+                encryption_state:
                   is: "encrypted"
         state: "present"
 
